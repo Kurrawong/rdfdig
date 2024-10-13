810f85a298b96bdcdc875de1ad5b7265bb2a4f7a
Lawon Lewis <lawson@kurrawong.ai>
2024-10-13 20:25:05 +1000

    chore: test docstrings

--- tests/test_all.py
+++ tests/test_all.py
@@ -7,12 +7,14 @@ from rdfdig.core import Diagram
 
 
 def test_file_loader():
+    """Test that data can be loaded from a file."""
     file = Path(__file__).parent / "data" / "lawson.ttl"
     diagram = Diagram()
     diagram.parse(source=file)
 
 
 def test_folder_loader():
+    """Test that data can be loaded from a folder."""
     folder = Path(__file__).parent / "data"
     diagram = Diagram()
     diagram.parse(source=folder)
@@ -20,10 +22,12 @@ def test_folder_loader():
 
 @pytest.mark.skip(reason="not implemented")
 def test_sparql_loader():
+    """Test that data can be loaded from a sparql endpoint."""
     assert True
 
 
 def test_serialization():
+    """Test that data can be serialized to JSON."""
     file = Path(__file__).parent / "data" / "lawson.ttl"
     diagram = Diagram()
     diagram.parse(source=file)
@@ -32,6 +36,7 @@ def test_serialization():
 
 
 def test_prefix_expansion():
+    """Test that a prefix can be expanded."""
     file = Path(__file__).parent / "data" / "lawson.ttl"
     diagram = Diagram()
     diagram.parse(source=file, iri="schema:Person")
